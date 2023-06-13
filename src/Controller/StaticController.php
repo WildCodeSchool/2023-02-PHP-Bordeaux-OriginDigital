9d228471526b3529a528978d03f36eb7d051a2f4
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-06-13 14:21:00 +0200

    add legal-notice

---
+++ src/Controller/StaticController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StaticController extends AbstractController
+{
+    #[Route('/legal-notice', name: 'legal-notice')]
+    public function index(): Response
+    {
+        return $this->render('static-pages/legal-notice.html.twig');
+    }
+}
