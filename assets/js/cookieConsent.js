fc8a56f72769fa20b594c1d56a1ae254e0ba30b0
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-06-17 14:52:27 +0200

    add decline cookies

--- assets/js/cookieConsent.js
+++ assets/js/cookieConsent.js
@@ -3,12 +3,20 @@ import Cookies from 'js-cookie';
 document.addEventListener("DOMContentLoaded", function () {
     const cookieConsentPopup = document.getElementById("cookie-consent-popup");
     const cookieConsentAccept = document.getElementById("cookie-consent-accept");
+    const cookieConsentDecline = document.getElementById("cookie-consent-decline");
 
     cookieConsentAccept.addEventListener("click", function () {
         // L'utilisateur a accepté les cookies
         cookieConsentPopup.style.display = "none";
         // Enregistrez le consentement de l'utilisateur dans un cookie
         Cookies.set('cookie_consent', 'true', { expires: 365 }); // Le cookie expire après 365 jours
+    });
+
+    cookieConsentDecline.addEventListener("click", function () {
+        // L'utilisateur a refusé les cookies
+        cookieConsentPopup.style.display = "none";
+        // Supprimez le cookie de consentement existant
+        Cookies.remove('cookie_consent');
     });
 
     // Vérifiez si l'utilisateur a déjà donné son consentement en vérifiant le cookie
@@ -18,6 +26,8 @@ document.addEventListener("DOMContentLoaded", function () {
         cookieConsentPopup.style.display = "block";
     }
 });
+
+
 
 
 
