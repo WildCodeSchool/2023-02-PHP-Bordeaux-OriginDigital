aced79a78a795166ceb1833d6659c03d4e578eba
Estèphe Fath <estephefath@gmail.com>
2023-06-27 16:32:55 +0200

    fix the contact page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,7 +7,6 @@ use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
