78f966e0ec217a81eea6b44eda45ee915a91486e
Estèphe Fath <estephefath@gmail.com>
2023-06-27 16:32:55 +0200

    fix the contact page

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
