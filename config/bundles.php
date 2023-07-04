f62737e20cadc0dc25cd4c52e70bfb9dec13868e
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-07-04 15:37:42 +0200

    conflits

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Andante\TimestampableBundle\AndanteTimestampableBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
