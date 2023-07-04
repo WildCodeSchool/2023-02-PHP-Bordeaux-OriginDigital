aa7cc543ff62be07e00b18bcf8d7036611837314
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-07-04 15:48:21 +0200

    conflits

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
