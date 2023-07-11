6a83db0ac3367d1f05fcfc82429ef081934bdb1c
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-07-11 20:43:32 +0200

    add background

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('cookieConsent', './assets/js/cookieConsent.js')
+    .addStyleEntry('admin', './assets/styles/admin.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
