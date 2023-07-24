9f7453036f717251a4b6ebfc566f3d63867848ac
EdouardH <ehsurfdkr@hotmail.com>
2023-07-24 11:10:08 +0200

    include clickable links in the GDPR text

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -72,9 +72,7 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Vous devez accepter les termes du contrat.',
                     ]),
                 ],
-                'label' => 'J\'accepte les Conditions Générales d\'Utilisation et reconnais avoir été informé que mes
-                 données personnelles seront utilisées tel que décrit ci-dessous et détaillé dans la Politique de
-                  protection des données personnelles du site Wild Sports',
+                'label' => 'J\'accepte les termes du contrat.',
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
